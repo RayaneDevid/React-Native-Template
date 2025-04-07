e45ac331064babd3fd2ddbccb2f66e02500b6c84
Rayane ELOUDJEDI <reloudjedi@dev-id.fr>
2025-04-07 16:50:28 +0200

    fix : using json files for translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,32 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import enTranslations from './locales/en.json';
+import frTranslations from './locales/fr.json';
 
 const resources = {
   en: {
-    translation: {
-      welcome: 'Welcome to React Native Template',
-      getStarted: 'Get started by editing src/screens/HomeScreen.tsx',
-      start: 'Start',
-      settings: 'Settings',
-      language: 'Language',
-      english: 'English',
-      french: 'French',
-      home: 'Home',
-      back: 'Back',
-    },
+    translation: enTranslations,
   },
   fr: {
-    translation: {
-      welcome: 'Bienvenue dans le Template React Native',
-      getStarted: 'Commencez par éditer src/screens/HomeScreen.tsx',
-      start: 'Commencer',
-      settings: 'Paramètres',
-      language: 'Langue',
-      english: 'Anglais',
-      french: 'Français',
-      home: 'Accueil',
-      back: 'Retour',
-    },
+    translation: frTranslations,
   },
 };
 
